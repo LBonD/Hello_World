9bf14368b877d9d08b1e71f92b27cbb9d8d9f9d7
LBonD <47715190+LBonD@users.noreply.github.com>
2019-02-17 17:27:45 +0300

    Add files via upload

---
+++ Hello_World.java
@@ -0,0 +1,12 @@
+package GB.lesson_1.hello_world;
+
+import javax.swing.*;
+
+public class Hello_World {
+
+    public static void main(String[] args) {
+	System.out.println("Hello World!!");
+        JOptionPane.showMessageDialog(null, "Hello World!!");
+
+    }
+}
